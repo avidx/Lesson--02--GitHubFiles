3e041cf7bb8cc93ec1a231ccf6d5d5efa009a2e6
Ubuntu <avinash.singh@girikon.com>
2019-09-24 12:09:13 +0000

    pushing java file

--- HelloJava.java
+++ HelloJava.java
@@ -2,7 +2,7 @@ class HelloJava {
 
 public static void main(String args[]){
 
-System.out.Println("I ma your Java Program. Than You!");
+System.out.Println("I ma your Java Program. Than You!!");
 }
 
 }
